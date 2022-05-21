6c7c7f82f887e36d295cfbb6cca9ec73b77be4fa
Ngo Tuong Vu <tuongvutbtcv@gmail.come.com>
2022-05-21 11:56:04 +0700

    combobox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,12 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name='Vũ';
   public age= 23;
+  public mau=['xanh','vàng','đỏ','lục','lam'];
+  public mau1=[{ten:'xanh',english:'green'},
+        {ten:'vàng',english:'yellow'},
+        {ten:'đỏ',english:'red'},
+        {ten:'lục',english:'Luc'},
+        {ten:'lam',english:'blue'}];
   constructor() { }
-
-  ngOnInit(): void {
+  public district:any[]=[];
+  public cities = [{city:'chọn thành phố', district:[]} ,{ city: 'Quảng Nam', district:['Thành Phố Hội An','Thành Phố Tam Kỳ']},{city:'TP HCM', district:['quan1','quan 2']}];
+  public ngOnInit(): void {
+    console.log('cities =',this.cities);
   }
   public resetName():void{
       console.log('resetName');
       this.name='Vũ';
   }
+  public changeCity(event:any){
+    const city = event.target.value;
+    console.log('event',city);
+    const search = this.cities.filter(data=>data.city === city);
+    console.log('search',search);
+    if (search && search.length>0){
+      this.district=search[0].district;
+    }
+}
 }
